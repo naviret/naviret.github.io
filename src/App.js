f5058a459f2698ded82e2141af2ba8c44c79e2e6
naviret <111366578+naviret@users.noreply.github.com>
2023-12-25 19:46:34 -0500

    setup webpages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import React from 'react';
+import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage.jsx';
+import ExperiencesPage from './pages/ExperiencesPage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<HomePage />}></Route>
+				<Route path='/home' element={<HomePage />}></Route>
+				<Route path='/projects' element={<ProjectsPage />}></Route>
+				<Route
+					path='/experiences'
+					element={<ExperiencesPage />}
+				></Route>
+				<Route path='/about' element={<AboutPage />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/ExperiencesPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ExperiencesPage() {
+	return <div>ExperiencesPage</div>;
+}
+
+export default ExperiencesPage;
