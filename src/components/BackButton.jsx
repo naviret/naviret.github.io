d64f88b1ab3b931704fab4d2294385a00bc09bb6
naviret <111366578+naviret@users.noreply.github.com>
2023-12-30 17:18:31 -0500

    created a back button for different webpages with styling

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './BackButton.css';
+import { useNavigate } from 'react-router-dom';
+
+function BackButton() {
+	const navigate = useNavigate();
+
+	function handleClick() {
+		navigate('/');
+	}
+
+	return (
+		<div className='button-container'>
+			<div className='button'>
+				<div className='button-text' onClick={() => handleClick()}>
+					{'<-- Home \\\\'}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default BackButton;
