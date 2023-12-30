be283f2e20e203082b20dd579921be898136ec93
naviret <111366578+naviret@users.noreply.github.com>
2023-12-30 17:17:24 -0500

    completed project component with styling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Project.css';
 import CodeBlock from './CodeBlock';
+import GithubIcon from '../assets/icons/github-drawn.png';
 
 function Project(props) {
 	return (
-		<div className='project-container'>
-			<div className='project-title'>{props.title}</div>
+		<div className='project-container' style={{ width: '50%' }}>
+			<div className='project-header'>
+				<div className='project-title'>
+					<a href={props.link} target='_blank'>
+						{props.title}
+					</a>
+				</div>
+				{/* <div className='github-icon'>
+					<a href='https://github.com/naviret' target='_blank'>
+						<img src={GithubIcon} alt='Github'></img>
+					</a>
+				</div> */}
+			</div>
 			<div className='project-image'>
 				<img src={props.img} alt={props.imgAlt}></img>
 			</div>
