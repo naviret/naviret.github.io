9e85b0d87b278c91a2e7ba92193f03ef55f618e1
naviret <111366578+naviret@users.noreply.github.com>
2023-12-31 17:26:06 -0500

    setup and styling for AboutPage and ExperiencesPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import './AboutPage.css';
+import PageHeader from '../components/PageHeader';
+import BackButton from '../components/BackButton';
 
 function AboutPage() {
-	return <div>AboutPage</div>;
+	return (
+		<div className='about-page'>
+			<div className='about-heading'>
+				<BackButton />
+				<div className='about-page-title'>
+					<PageHeader name='About Me' />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AboutPage;
--- src/pages/ExperiencesPage.jsx
+++ src/pages/ExperiencesPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import './ExperiencesPage.css';
+import PageHeader from '../components/PageHeader';
+import BackButton from '../components/BackButton';
 
 function ExperiencesPage() {
-	return <div>ExperiencesPage</div>;
+	return (
+		<div className='experiences-page'>
+			<div className='experiences-heading'>
+				<BackButton />
+				<div className='experiences-page-title'>
+					<PageHeader name='Experiences' />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExperiencesPage;
