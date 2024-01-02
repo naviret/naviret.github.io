fb068d6adf97f9253ef72388b675654e8eaeaddc
naviret <111366578+naviret@users.noreply.github.com>
2024-01-02 10:18:37 -0500

    using HashRouter instead of BrowserRouter for deploying to Github

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage.jsx';
 import ExperiencesPage from './pages/ExperiencesPage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path='/' element={<HomePage />}></Route>
 				<Route path='/home' element={<HomePage />}></Route>
@@ -19,7 +19,7 @@ function App() {
 				></Route>
 				<Route path='/aboutme' element={<AboutPage />}></Route>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
